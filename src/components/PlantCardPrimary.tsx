d75ac564bdbccf78f093bde925e3db08f2d4e087
paulovitor21 <paulovitorpereira21@outlook.com>
2021-04-22 16:32:24 -0400

    Dia 3 - PlantSelect concluído

--- src/components/PlantCardPrimary.tsx
+++ src/components/PlantCardPrimary.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {
-    StyleSheet,
-    Text
-} from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+
 import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { SvgFromUri } from 'react-native-svg';
+
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
@@ -21,6 +21,12 @@ export const PlantCardPrimary = ({ data, ...rest }: PlantProps) => {
             style={styles.container}
             {...rest}
         >
+            <SvgFromUri
+                uri={data.photo}
+                width={70}
+                height={70}
+            />
+
             <Text style={styles.text}>
                 {data.name}
             </Text>
--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -3,12 +3,15 @@ import {
     View,
     Text,
     StyleSheet,
-    FlatList
+    FlatList,
+    ActivityIndicator
 } from 'react-native';
 import { EnviromentButton } from '../components/EnviromentButton';
 
 import { Header } from '../components/Header';
 import { PlantCardPrimary } from '../components/PlantCardPrimary';
+import { Load } from '../components/Load';
+
 import api from '../services/api';
 
 import colors from '../styles/colors';
@@ -35,10 +38,61 @@ interface PlantProps {
 export function PlantSelect() {
     const [enviroments, setEnviroments] = useState<EnviromentProps[]>([]);
     const [plants, setPlants] = useState<PlantProps[]>([]);
+    const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
+    const [enviromentSelected, setEnviromentSelected] = useState('all');
+    const [loading, setLoading] = useState(true);
+
+    const [page, setPage] = useState(1);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [loadedAll, setLoadedAll] = useState(false);
+
+    function handleEnviromentSelected(environment: string) {
+        setEnviromentSelected(environment);
+
+        if (environment == 'all')
+            return setFilteredPlants(plants);
+
+        const filtered = plants.filter(plant =>
+            plant.environments.includes(environment)
+        );
+
+        setFilteredPlants(filtered);
+    }
+
+    async function fetchPlants() {
+        const { data } = await api
+            .get(`plants?_sort=name&_order=asc&_page=${page}&_limit=8`);
+
+        if (!data)
+            return setLoading(true);
+
+        if (page > 1) {
+            setPlants(oldValue => [...oldValue, ...data])
+            setFilteredPlants(oldValue => [...oldValue, ...data])
+        } else {
+            setPlants(data);
+            setFilteredPlants(data);
+        }
+
+        setLoading(false);
+        setLoadingMore(false);
+
+    }
+
+    function handleFetchMore(distance: number) {
+        if (distance < 1)
+            return;
+
+        setLoadingMore(true);
+        setPage(oldValue => oldValue + 1);
+        fetchPlants();
+
+    }
 
     useEffect(() => {
         async function fetchEnviroment() {
-            const { data } = await api.get('plants_environments');
+            const { data } = await api
+                .get('plants_environments?_sort=title&_order=asc');
             setEnviroments([
                 {
                     key: 'all',
@@ -51,12 +105,12 @@ export function PlantSelect() {
     }, [])
 
     useEffect(() => {
-        async function fetchPlants() {
-            const { data } = await api.get('plants');
-            setPlants(data);
-        }
+
         fetchPlants();
     }, [])
+
+    if (loading)
+        return <Load />
 
     return (
         <View style={styles.container}>
@@ -78,6 +132,8 @@ export function PlantSelect() {
                     renderItem={({ item }) => (
                         <EnviromentButton
                             title={item.title}
+                            active={item.key === enviromentSelected}
+                            onPress={() => handleEnviromentSelected(item.key)}
 
                         />
                     )}
@@ -89,12 +145,21 @@ export function PlantSelect() {
 
             <View style={styles.plants}>
                 <FlatList
-                    data={plants}
+                    data={filteredPlants}
                     renderItem={({ item }) => (
                         <PlantCardPrimary data={item} />
                     )}
                     showsVerticalScrollIndicator={false}
                     numColumns={2}
+                    onEndReachedThreshold={0.1}
+                    onEndReached={({ distanceFromEnd }) =>
+                        handleFetchMore(distanceFromEnd)
+                    }
+                    ListFooterComponent={
+                        loadingMore
+                            ? <ActivityIndicator color={colors.green} />
+                            : <></>
+                    }
                 />
             </View>
         </View>
@@ -133,6 +198,7 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingHorizontal: 32,
         justifyContent: 'center'
-    }
-
+    },
 });
+
+//#astronautas
